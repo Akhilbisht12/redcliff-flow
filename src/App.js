a785cae071d913ec0508f892e312520ce60b71b7
Akhil Bisht <akhilbisht83@gmail.com>
2022-11-15 13:19:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [fields, setFields] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  const startRedcliffeFlow = async (e) => {
+    e.preventDefault();
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(fields);
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    try {
+      await fetch("https://backend.aretehealth.tech/api/v1/customer/redcliffe", requestOptions);
+      alert("Submitted.");
+      setFields({
+        name: "",
+        email: "",
+        phone: "",
+      });
+    } catch (error) {
+      alert("Failure! Try Again.");
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="">
+          <form className="form" onSubmit={startRedcliffeFlow}>
+            <img
+              className="logo"
+              alt="redcliff logo"
+              src="https://staticredcliffelabs.s3.amazonaws.com/media/gallary-file/None/bf8ae4e8-1f26-45a6-8011-938ad23ad65a.svg"
+            />
+            <input
+              placeholder="Name"
+              value={fields.name}
+              onChange={(e) => setFields({ ...fields, name: e.target.value })}
+              className="input"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={fields.email}
+              onChange={(e) => setFields({ ...fields, email: e.target.value })}
+              className="input"
+            />
+
+            <input
+              type="tel"
+              placeholder="Whatsapp Number"
+              value={fields.phone}
+              onChange={(e) => setFields({ ...fields, phone: e.target.value })}
+              className="input"
+            />
+
+            <input className="button" type="submit" />
+          </form>
+        </div>
       </header>
     </div>
   );
